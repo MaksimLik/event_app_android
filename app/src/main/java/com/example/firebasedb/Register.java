3d5a7ee58eef2cae67c46ef79693b39d32f51a5b
Maksym <72620745+MaksimLik@users.noreply.github.com>
2023-06-17 12:45:28 +0300

    the little changes

--- app/src/main/java/com/example/firebasedb/Register.java
+++ app/src/main/java/com/example/firebasedb/Register.java
@@ -1,18 +1,14 @@
 package com.example.firebasedb;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
